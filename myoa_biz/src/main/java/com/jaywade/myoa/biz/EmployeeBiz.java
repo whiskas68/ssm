979b8b15f7f620ce94689c80d89450695ccc4ed2
whiskas68 <whiskas.sevenclouds@gmail.com>
2019-04-10 22:10:40 +0800

    add employee

---
+++ myoa_biz/src/main/java/com/jaywade/myoa/biz/EmployeeBiz.java
@@ -0,0 +1,13 @@
+package com.jaywade.myoa.biz;
+
+import com.jaywade.myoa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeBiz {
+    void add(Employee employee);
+    void edit(Employee employee);
+    void remove(String sn);
+    Employee get(String sn);
+    List<Employee> getAll();
+}
